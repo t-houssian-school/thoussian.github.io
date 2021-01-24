b40ccde95aecee116d1191fcbd44b54736bbd2eb
t-houssian <tylerhoussian@gmail.com>
2021-01-23 21:25:43 -0700

    lesson 3

---
+++ lesson2/scripts/page.js
@@ -0,0 +1,7 @@
+let year = new Date().getFullYear()
+
+document.querySelector('#year').textContent = year
+
+let mod = document.lastModified;
+
+document.querySelector('#instupdated').textContent = mod
