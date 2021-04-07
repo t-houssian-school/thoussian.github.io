ec64ad672f8baa23e16d94e71769e3c673011fe1
t-houssian <tylerhoussian@gmail.com>
2021-04-06 23:41:07 -0600

    Finished Final Site

--- temple-inn-and-suites/js/temple.js
+++ temple-inn-and-suites/js/temple.js
@@ -1,4 +1,9 @@
 const requestURL = 'js/temples.json';
+const requestURL2 = 'https://api.openweathermap.org/data/2.5/weather?id=5542758&units=imperial&appid=479fe9277fff4b68adc83def2d8c2a98';
+const requestURL3 = 'https://api.openweathermap.org/data/2.5/weather?id=3530597&units=imperial&appid=479fe9277fff4b68adc83def2d8c2a98';
+const requestURL4 = 'https://api.openweathermap.org/data/2.5/weather?id=1690295&units=imperial&appid=479fe9277fff4b68adc83def2d8c2a98';
+const requestURL5 = 'https://api.openweathermap.org/data/2.5/weather?id=1819729&units=imperial&appid=479fe9277fff4b68adc83def2d8c2a98';
+
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -36,55 +41,93 @@ function showSlides(n) {
   })
   .then(function (jsonObject) {
     const temples = jsonObject;
-
-    let card = document.createElement('section');
-    let h2 = document.querySelector('#temple-name');
-    let info = document.querySelector('.temple-info');
-    info.removeChild(info.childNodes[0])
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let p4 = document.createElement('p');
-    let p5 = document.createElement('p');
-    let p6 = document.createElement('p');
-    let p7 = document.createElement('p');
-    let h3 = document.createElement('h3')
-    h3.textContent = "Temple Info"
-
-    let ul = document.createElement('ul');
-    let li = document.createElement('li');
-    li.innerHTML = "<br>Closures: ";
-    ul.appendChild(li);
-
-    h2.textContent = temples[slideI].name
-    p1.textContent = "Address: " + temples[slideI].address1 + " " + temples[slideI].city + ", " + temples[slideI].state + " " + temples[slideI].zip
-    p2.textContent = "Email: " + temples[slideI].email
-    p3.textContent = "Phone: " + temples[slideI].phone
-    p4.textContent = "Services: " + temples[slideI].services
-    p5.textContent = "Ordinance Schedule: " + temples[slideI].ordinanceschedule
-    p6.textContent = "Session Schedule: " + temples[slideI].sessionschedule
-    for(let x = 0; x < temples[slideI].templeclosureschedule.length; x++) {
-        let li = document.createElement('li');
-        li.innerHTML = temples[slideI].templeclosureschedule[x];
-        ul.appendChild(li);
+    
+    var weather;
+    var min;
+    var max;
+    let request;
+    if(slideI == 0){
+      request = requestURL2
     }
+    else if(slideI == 1){
+      request = requestURL3
+    }
+    else if(slideI == 2){
+      request = requestURL4
+    }
+    else{
+      request = requestURL5
+    }
+    fetch(request)
+    .then( response => { return response.json(); } )
+    .then(  jsonObject => { 
+        table = jsonObject;
 
-    let his = document.createElement('h3')
-    his.textContent = "History"
-    p7.textContent =  temples[slideI].history
+        weather = table.main.temp;
+        min = table.main.temp_min;
+        max = table.main.temp_max
 
-    card.appendChild(h3)
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
-    card.appendChild(p4);
-    card.appendChild(p5);
-    card.appendChild(p6);
-    card.appendChild(ul)
-    card.appendChild(his);
-    card.appendChild(p7);
+        let card = document.createElement('section');
+        let h2 = document.querySelector('#temple-name');
+        let info = document.querySelector('.temple-info');
+        info.removeChild(info.childNodes[0])
+        let pw = document.createElement('p')
+        let pmin = document.createElement('p')
+        let pmax = document.createElement('p')
+        let pt = document.createElement('h3')
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let p6 = document.createElement('p');
+        let p7 = document.createElement('p');
+        let h3 = document.createElement('h3')
+        h3.textContent = "Temple Info"
 
-    info.appendChild(card)
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        li.innerHTML = "<br>Closures: ";
+        ul.appendChild(li);
+
+        pt.textContent = "Current Weather"
+        pw.textContent = "Current Temperature: " + weather + "°F"
+        pmin.textContent = "Low: " + min + "°F"
+        pmax.textContent = "High: " + max + "°F"
+        h2.textContent = temples[slideI].name
+        p1.textContent = "Address: " + temples[slideI].address1 + " " + temples[slideI].city + ", " + temples[slideI].state + " " + temples[slideI].zip
+        p2.textContent = "Email: " + temples[slideI].email
+        p3.textContent = "Phone: " + temples[slideI].phone
+        p4.textContent = "Services: " + temples[slideI].services
+        p5.textContent = "Ordinance Schedule: " + temples[slideI].ordinanceschedule
+        p6.textContent = "Session Schedule: " + temples[slideI].sessionschedule
+        for(let x = 0; x < temples[slideI].templeclosureschedule.length; x++) {
+            let li = document.createElement('li');
+            li.innerHTML = temples[slideI].templeclosureschedule[x];
+            ul.appendChild(li);
+        }
+
+        let his = document.createElement('h3')
+        his.textContent = "History"
+        p7.textContent =  temples[slideI].history
+
+        card.appendChild(pt)
+        card.appendChild(pw)
+        card.appendChild(pmin)
+        card.appendChild(pmax)
+        card.appendChild(h3)
+        card.appendChild(p1);
+        card.appendChild(p2);
+        card.appendChild(p3);
+        card.appendChild(p4);
+        card.appendChild(p5);
+        card.appendChild(p6);
+        card.appendChild(ul)
+        card.appendChild(his);
+        card.appendChild(p7);
+
+        info.appendChild(card)
+    })
 
     });
 }
@@ -94,7 +137,6 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); 
     const temples = jsonObject;
 
     let im1 = document.querySelector('#im1');
